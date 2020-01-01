888817eca00452143a8debc93e08a1bbb0167cd0
Zasu Pitts <grafpoo@gmail.com>
2020-01-01 18:25:06 -0500

    get jpa in pom

---
+++ profile-mvc/src/main/java/liveproject/m2k8s/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package liveproject.m2k8s.config;
+
+import liveproject.m2k8s.Application;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:persistence.properties")
+//@PropertySource("classpath:application.properties")
+//@ComponentScan(basePackageClasses = Application.class)
+class ApplicationConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}
