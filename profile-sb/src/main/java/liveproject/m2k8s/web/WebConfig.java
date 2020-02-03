993f841f23654e77bd6464a582ec9a503a269191
Zasu Pitts <grafpoo@gmail.com>
2020-02-03 06:20:05 -0500

    WIP - static woes

--- profile-sb/src/main/java/liveproject/m2k8s/web/WebConfig.java
+++ profile-sb/src/main/java/liveproject/m2k8s/web/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
@@ -43,5 +44,10 @@ public class WebConfig implements WebMvcConfigurer {
   public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
     configurer.enable();
   }
-  
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/public/**").addResourceLocations("/public/");
+  }
+
 }
