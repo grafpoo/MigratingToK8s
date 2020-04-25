c8fdae62c67098460bcbbcc373a9393fd57069a0
Zasu Pitts <grafpoo@gmail.com>
2020-04-25 16:25:50 -0400

    changed name, changed data dir

--- milestone-1/step-3/src/main/java/liveproject/m2k8s/Profile.java
+++ milestone-1/step-3/src/main/java/liveproject/m2k8s/Profile.java
@@ -2,6 +2,7 @@ package liveproject.m2k8s;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -22,15 +23,15 @@ public class Profile {
   @Id
   @GeneratedValue(strategy=GenerationType.IDENTITY)
   private Long id;
-  
+
   @NotNull
-  @Size(min=4, max=16)
+  @Size(min=5, max=16)
   private String username;
 
   @NotNull
   @Size(min=5, max=25)
   private String password;
-  
+
   @NotNull
   @Size(min=2, max=30)
   private String firstName;
@@ -38,7 +39,7 @@ public class Profile {
   @NotNull
   @Size(min=2, max=30)
   private String lastName;
-  
+
   @NotNull
   @Email
   private String email;
