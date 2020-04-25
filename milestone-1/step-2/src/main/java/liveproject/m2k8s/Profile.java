90c88af7ec657a9f5c1321f1a1ad58d49d6b91ca
Zasu Pitts <grafpoo@gmail.com>
2020-04-25 15:19:30 -0400

    cleaned up, fixed jpa

--- milestone-1/step-2/src/main/java/liveproject/m2k8s/Profile.java
+++ milestone-1/step-2/src/main/java/liveproject/m2k8s/Profile.java
@@ -2,6 +2,7 @@ package liveproject.m2k8s;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -20,7 +21,7 @@ import org.hibernate.validator.constraints.Email;
 public class Profile {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
   private Long id;
   
   @NotNull
